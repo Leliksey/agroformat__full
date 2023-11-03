51647912f983ad406974e09a9cbf55ee0191a192
Богдан Голєв <doctorhause345@gmail.com>
2023-11-03 19:08:54 +0200

    анімація головної сторінки

--- js/animation.js
+++ js/animation.js
@@ -35,4 +35,47 @@ $(document).ready(function() {
   $(".cart").mouseleave(function(){
     $('.cart__modal').slideUp(200);
   });
+
+  function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+        change.target.classList.add('element-show');
+      }
+    });
+  };
+  let options = {};
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('.elementAppears');
+  for (let elm of elements) {
+    observer.observe(elm);
+  };
+
+  function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+        change.target.classList.add('element-show');
+      }
+    });
+  };
+  let options1 = {};
+  let observer1 = new IntersectionObserver(onEntry, options1);
+  let elements1 = document.querySelectorAll('.textAppears__right');
+  for (let elm of elements1) {
+    observer1.observe(elm);
+  };
+
+  function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+        change.target.classList.add('element-show');
+      }
+    });
+  };
+  let options2 = {};
+  let observer2 = new IntersectionObserver(onEntry, options2);
+  let elements2 = document.querySelectorAll('.textAppears__left');
+  for (let elm of elements2) {
+    observer2.observe(elm);
+  };
+
 })
